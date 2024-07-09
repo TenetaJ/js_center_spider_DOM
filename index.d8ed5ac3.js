c82f1d752f78ce75b65fe59b81da52e91fa4d94a
unknown <tenetaevgen@gmail.com>
2024-07-09 15:34:19 +0300

    make build

---
+++ index.d8ed5ac3.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall").getBoundingClientRect(),e=document.querySelector(".spider");e.style.left="".concat(t.width/2,"px"),e.style.top="".concat(t.height/2,"px"),e.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.d8ed5ac3.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const wallCoords = wall.getBoundingClientRect();
+const spider = document.querySelector(".spider");
+spider.style.left = `${wallCoords.width / 2}px`;
+spider.style.top = `${wallCoords.height / 2}px`;
+spider.style.transform = "translate(-50%, -50%)";
+
+//# sourceMappingURL=index.f75de5e1.js.map
